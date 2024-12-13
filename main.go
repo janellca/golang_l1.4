e35b557b0866ffaf03a551e24d34bf0a45ac2708
Modlizhka <fierce_type@tutamail.com>
2024-12-13 08:24:43 +0700

    release

--- main.go
+++ main.go
@@ -2,26 +2,25 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"time"
 )
 
-func main() {
-	var myMap sync.Map // Используем sync map
-
-	arr1 := [...]string{"2", "4", "6", "8", "10"}
-	arr2 := [...]int{2, 4, 6, 8, 10}
-
-	var wg sync.WaitGroup
-	for i := range arr1 {
-		wg.Add(1)
-		go func(key string, value int) { // создаем горутину и записываем значение в map
-			defer wg.Done()
-			myMap.Store(arr1[i], arr2[i])
-		}(arr1[i], arr2[i])
+func worker(stop <-chan bool) {
+	for {
+		select {
+		default:
+			fmt.Println("The worker is working")
+		case <-stop:
+			fmt.Println("The worker is stopped")
+			return
+		}
 	}
-	wg.Wait() // ожидаем завершения работы всех горутин
-	myMap.Range(func(key, value interface{}) bool {
-		fmt.Printf("Ключ: %v, Значение: %v\n", key, value)
-		return true // Возврат true продолжает перебор
-	})
+}
+
+func main() {
+	stop := make(chan bool)
+	go worker(stop)
+
+	time.Sleep(2 * time.Second)
+	stop <- true
 }
